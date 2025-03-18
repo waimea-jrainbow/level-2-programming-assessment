8d541eafe650450ba33ca6abc95ca056d64bfa53
Raven Rainbow <jdrainbow@waimea.school.nz>
2025-03-18 18:11:59 +1300

    funs and comments
    began to layout comments to plan what i need to do and set up basic functions for getting different types etc

--- src/Main.kt
+++ src/Main.kt
@@ -30,6 +30,93 @@
 
 
 fun main() {
-    println("Hello World!")
+    //intro
+    println("--Welcome to Battle of the century--")
+    println("--This is a game for 2 players--")
+    //rules
+    println("These are the rules:")
+    println("")
+    //player 1 setup
+
+        // name
+
+        //class choice
+            //list classes
+
+        //weapon choice
+
+    //player 2 setup
+
+    // name
+
+    //class choice
+        //list classes
+
+    //weapon choice
+
 }
 
+fun setupPlayer{
+
+}
+
+fun getplayerName{
+
+}
+
+fun chooseClass{
+
+}
+
+/**
+ * Function to get a string from the user
+ *
+ * parameters:
+ * prompt:
+ * -text to show user
+ * returns:
+ * -string that the user has entered
+ */
+
+fun getString(prompt: String): String {
+    var userinput: String
+
+    while (true) {
+        print(prompt)
+        userinput = readln()
+        if (userinput.isNotBlank()) break
+
+    }
+    return userinput
+}
+
+/**
+ * Function to get an integer from the user
+ *
+ * parameters:
+ * prompt:
+ * -text to show user
+ * returns:
+ * -integer that the user has entered
+ */
+fun getInt(prompt: String): Int {
+    var intValue:Int?
+
+    while (true) {
+        val userinput = getString(prompt)
+        intValue = userinput.toIntOrNull()
+        if (intValue != null) break
+
+    }
+
+    return intValue!!
+
+}
+
+fun getChar(prompt: String): Char {
+    while (true) {
+        val userinput = getString(prompt).first()
+        if ("".contains(userinput)) return userinput
+        println("Please enter a valid  (e.g. )")
+    }
+}
